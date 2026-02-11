580c045e4859c3d018daf0e3e58f0845b1e6cd6e
Ismaahmed <muhiza53@gmail.com>
2026-02-11 20:45:23 +0300

    commit

--- App.py
+++ App.py
@@ -3,6 +3,7 @@ CSV_PATH = './Lifestyle_Sleep_Dataset.csv'
 df = pd.read_csv(CSV_PATH)
 
 df.columns = df.columns.str.strip()
+df = df.drop('Timestamp', axis=1)
 
 # print(df.head(10))
 # print(df.shape )
@@ -15,7 +16,7 @@ df.columns = df.columns.str.strip()
 # print(df[' Primary Social Media App '].head(10))
 
 
-df["Primary Social Media App"] = df["Primary Social Media App"].replace({"youtube": "YouTube"})
+df["Primary Social Media App"] = df["Primary Social Media App"].replace({"youtube": "YouTube"}).str.strip()
 # print(df['Primary Social Media App'].value_counts(dropna=False))
 
 df["How many days do you exercise per week?"] = df["How many days do you exercise per week?"].fillna(df["How many days do you exercise per week?"].mode()[0])
@@ -32,22 +33,44 @@ def iqr_fun(series, k=1.5):
     lower = q1 - k * iqr
     upper = q3 + k * iqr
     return lower, upper
+
+
 low_age, high_age = iqr_fun(df["Age"])
 low_daily, high_daily = iqr_fun(df["Daily Screen Time (Hours)"])
-low_sleep, high_sleep = iqr_fun(df["How many hours did you sleep last night?"])
-df["Age"] = df["Age"].clip(lower=low_age, upper=high_age).astype(int)
-df["How many hours did you sleep last night?"] = df["How many hours did you sleep last night?"].clip(lower=low_sleep, upper=high_sleep)
-df["Daily Screen Time (Hours)"] = df["Daily Screen Time (Hours)"].clip(lower=low_daily, upper=high_daily)
-print("age after th iq")
-print(df["Age"].describe())
+low_Excercise, high_Excercise = iqr_fun(df["How many days do you exercise per week?"])
 
-# print("IQR of age:")
-# print("low age:", low_age, "high_age:", high_age)
+df["Age"] = df["Age"].clip(lower=low_age, upper=high_age).astype(int)
+df["Daily Screen Time (Hours)"] = df["Daily Screen Time (Hours)"].clip(lower=low_daily, upper=high_daily)
+
+df["How many days do you exercise per week?"] = df["How many days do you exercise per week?"].clip(lower=low_Excercise, upper=high_Excercise)
+
+# print("after clipping")
+# print(df["Daily Screen Time (Hours)"].describe())
+
 
 # print("IQR of Daily Screen Time (Hours):")
 # print("low_daily:", low_daily, "high_daily:", high_daily)
 # print("IQR of how How many hours did you sleep last night?:")
 # print("low_sleep:", low_sleep, "high_sleep:", high_sleep)
+
+
+exercise_per_week = {
+    "0":0,
+    "1-2 days":1.5,
+     "3-4 days":3.5,
+     "5+ days": 5
+
+}
+df["How many days do you exercise per week?"] = df["How many days do you exercise per week?"].map(exercise_per_week)
+# print(df["How many days do you exercise per week?"].value_counts())
+
+df = pd.get_dummies(df, columns=["Primary Social Media App"], drop_first=False)
+print("One hot encoding Primary Social Media App:")
+# print([c for c in df.columns if c.startswith("Primary Social Media App")])
+# print(df.head(10))
+
+
+
 
 
 
