db64db5733de93b6e249107e28135a86960278e6
Ismaahmed <muhiza53@gmail.com>
2026-02-13 20:21:11 +0300

    commit

---
+++ script.py
@@ -0,0 +1,29 @@
+a,b = 15,20
+if a > b :
+    print("a geater then")
+elif a < b :
+    print("a loss then") 
+else : 
+    print("a equal")   
+# print(a >= b)
+# print(a <= b)
+
+age = 17
+
+if age > 13 and age < 19 :
+    print("teenager")
+else:
+    print("t")    
+print(age > 13 and age <= 19)
+
+x = 10
+
+if x < 5 or x > 15 :
+    print("")
+print(x < 5 or x > 15)  
+
+num = 7
+if num % 2 == 0 :
+    print("even")
+else :
+    print("odd")
